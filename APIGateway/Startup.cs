620c4703ce7d873df962203e76863fe164480bee
Maher Al Abbasi <Maher.AlAbbasi@isb-solutions.de>
2020-12-30 11:12:51 +0100

    Ocelot API Gateway mit PersonService verbinden

--- APIGateway/Startup.cs
+++ APIGateway/Startup.cs
@@ -2,20 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace APIGateway
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        [Obsolete]
+        public Startup(Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("Ocelot.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var authenticationProviderKey = "TestKey";
+            Action<JwtBearerOptions> options = o =>
+            {
+                o.Authority = "https://localhost:44373/";
+                o.Audience = "myresourceapi";
+                o.RequireHttpsMetadata = false;
+            };
+
+            services.AddAuthentication()
+                .AddJwtBearer(authenticationProviderKey, options);
+
+            services.AddOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +62,8 @@ namespace APIGateway
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            app.UseOcelot().Wait();
         }
     }
 }
